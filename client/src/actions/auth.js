5dc0acac9e96901e9603c4d048e3addb329d20b9
Daniel Susanu <d.susanu.development@gmail.com>
2019-05-06 22:19:52 +0100

    Added login and register redirect auter auth

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,6 +3,8 @@ import { setAlert } from './alert';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
   PROXY,
   USER_LOADED,
   AUTH_ERROR
@@ -43,6 +45,8 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
+
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -51,6 +55,36 @@ export const register = ({ name, email, password }) => async dispatch => {
     }
     dispatch({
       type: REGISTER_FAIL
+    });
+  }
+};
+
+// login user
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post(`${PROXY}/api/auth`, body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: LOGIN_FAIL
     });
   }
 };
